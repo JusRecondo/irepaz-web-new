ddf8bfc5eea1ef4ea1281450bd6856ac1082c5ba
JusRecondo <justina.recondo@gmail.com>
2025-09-05 20:55:03 -0300

    add marquee to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+import HomeMarquee from "@/components/home-marquee";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-       Hola
+    <div className="font-sans grid items-center justify-items-center min-h-screen p-0 gap-16 overflow-hidden">
+      <main className="w-full h-full flex flex-col gap-[32px] item-center justify-center">
+        <HomeMarquee>
+          <Link href="/solista" className="text-4xl w-full hover:text-amber-800">IRE PAZ SOLISTA</Link>
+        </HomeMarquee>
+        <HomeMarquee speed={55} delay={1} direction="right">
+          <Link href="/edificios" className="text-4xl w-full hover:text-purple-800">EDIFICIOS DERRUMBADOS</Link>
+        </HomeMarquee>
+        <HomeMarquee speed={45} delay={0.5}>
+          <Link href="/la-corteza-electrica" className="text-4xl w-full hover:text-orange-500">IRE PAZ Y LA CORTEZA ELÉCTRICA</Link>
+        </HomeMarquee>
+        <HomeMarquee speed={40} delay={2} direction="right">
+          <Link href="/ipjr" className="text-4xl w-full hover:text-teal-700">IPJR</Link>
+        </HomeMarquee>
+        <HomeMarquee speed={60}>
+          <Link href="/supervivencia-visual" className="text-4xl w-full hover:text-red-400">SUPERVIVENCIA VISUAL</Link>
+        </HomeMarquee>        
       </main>
     </div>
   );
---
+++ src/components/home-marquee.tsx
@@ -0,0 +1,19 @@
+import Marquee from 'react-fast-marquee'
+
+interface Props {
+  children: React.ReactNode
+  speed?: number
+  delay?: number
+  direction?: 'left' | 'right' | 'up' | 'down'
+}
+
+
+const HomeMarquee = ({ children, speed = 50, delay = 0, direction = 'left' }: Props) => {
+  return (
+    <Marquee className="overflow-hidden" speed={speed} delay={delay} direction={direction} gradient pauseOnHover gradientColor="#fbfbfb">
+      {children}
+    </Marquee>
+  )
+}
+
+export default HomeMarquee
