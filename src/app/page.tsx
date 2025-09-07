1c74103114ec24d86e6abe51ad28625d199a1110
JusRecondo <justina.recondo@gmail.com>
2025-09-07 11:15:18 -0300

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           <Link href="/solista" className="text-4xl w-full hover:text-amber-800">IRE PAZ SOLISTA</Link>
         </HomeMarquee>
         <HomeMarquee speed={55} delay={1} direction="right">
-          <Link href="/edificios" className="text-4xl w-full hover:text-purple-800">EDIFICIOS DERRUMBADOS</Link>
+          <Link href="/edificios" className="text-4xl w-full hover:text-purple-800">EDIFICIOS</Link>
         </HomeMarquee>
         <HomeMarquee speed={45} delay={0.5}>
           <Link href="/la-corteza-electrica" className="text-4xl w-full hover:text-orange-500">IRE PAZ Y LA CORTEZA ELÉCTRICA</Link>
